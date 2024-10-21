fa584bc4d5df8e982e66321231ebf0ae65b99773
matidan01 <matildedantino@gmail.com>
2024-10-21 18:31:59 +0200

    Update ThingInterface

--- src/thing-model/ThingInterface.ts
+++ src/thing-model/ThingInterface.ts
@@ -2,9 +2,21 @@ import { ExposedThing, Servient } from "@node-wot/core";
 
 export class ThingInterface extends ExposedThing {
 
-    constructor(servient: Servient, init: WoT.ExposedThingInit) {
-        super(servient, init); // Passa i parametri corretti alla classe base
+    eventTickRate : number = 0;
+    ticksFromLastEvent = 0;
+
+    constructor(servient: Servient, init: WoT.ExposedThingInit, eventTickRate : number) {
+        super(servient, init);
+        this.eventTickRate = eventTickRate;
     }
     
-    tick(): void {}
+    tick(): void {
+        this.ticksFromLastEvent++;  
+        if (this.eventTickRate == this.ticksFromLastEvent) {
+            this.tickEvent();
+            this.ticksFromLastEvent = 0;  
+        }
+    }
+
+    tickEvent(): void {}
 }
