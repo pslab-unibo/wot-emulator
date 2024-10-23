87e0206574157d605a2666a4b99f4f7d50d669d1
matidan01 <matildedantino@gmail.com>
2024-10-23 19:13:53 +0200

    Add Servient as a field in the scheduler implementation

--- src/simulation/scheduler.ts
+++ src/simulation/scheduler.ts
@@ -1,37 +1,50 @@
-import { ExposedThing } from "@node-wot/core";
+import Servient from "@node-wot/core";
 import { ThingInterface } from "../thing-model/ThingInterface";
+import { Content } from "@node-wot/core"; 
 
 export class Scheduler {
-
+    private servient: Servient;
     private period: number;
-    private things: Map<string, ThingInterface> = new Map<string, ThingInterface>();
 
-    constructor(period: number) {
+    constructor(period: number, servient: Servient) {
+        this.servient = servient; 
         this.period = period;
     }
 
     public addThing(thing: ThingInterface): boolean {
-        if (!this.things.has(thing.id)) {
-            this.things.set(thing.id, thing);
-            return true;
-        } else {
-            return false;
+        return this.servient.addThing(thing);
+    }
+
+    private wait(ms: number) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    public async schedule(): Promise<void> {
+        
+        try {
+            const things = this.servient.getThings();
+            
+            for (const thing of Object.values(things)) {
+                console.log(`Thing ID: ${thing.id}, Title: ${thing.title}, tick: ${thing.constructor.name}`);
+                const actionName = 'tick';
+
+                if (thing instanceof ThingInterface) {
+                    
+                    try {
+                        console.log(`Invoking action '${actionName}' for Thing: ${thing.id}`);
+                        await thing.tick();
+                    } catch (error) {
+                        console.log(`Error invoking action '${actionName}' for Thing '${thing.id}':`, error);
+                    }
+                } 
+            }
+        } catch (error) {
+            console.log('Error during scheduling:', error);
         }
+    
+        await this.wait(this.period);
+        
+        this.schedule();
     }
-
-    private getThings(): Record<string, WoT.ThingDescription> {
-        const ts: { [key: string]: WoT.ThingDescription } = {};
-        this.things.forEach((thing, id) => {
-            ts[id] = thing.getThingDescription();
-        });
-        return ts;
-    }
-
-    public schedule(): void {
-        this.things.forEach((thing, id) => {
-            if (typeof thing.tick === "function") {
-                thing.tick(); 
-            } 
-        });
-    }
+    
 }
