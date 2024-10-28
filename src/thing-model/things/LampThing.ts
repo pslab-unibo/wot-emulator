958469c75593a6a7411e254fc3bc1e3a0e3de725
matidan01 <matildedantino@gmail.com>
2024-10-28 16:44:39 +0100

    Add isOn filed and toggle method for LampThing

--- src/thing-model/things/LampThing.ts
+++ src/thing-model/things/LampThing.ts
@@ -3,7 +3,8 @@ import { ThingInterface } from "../ThingInterface";
 
 export class LampThing extends ThingInterface {
 
-    private intensity: number = 0; 
+    private intensity: number = 0;
+    private isOn: boolean = false;
 
     constructor(servient: Servient, init: WoT.ExposedThingInit, eventTickRate: number) {
         super(servient, init, eventTickRate);
@@ -20,11 +21,35 @@ export class LampThing extends ThingInterface {
                 throw new Error("Invalid intensity value. Must be between 0 and 100.");
             }
         });
+
+        this.getThing().setPropertyReadHandler("isOn", async () => {
+            return this.isOn;
+        });
+    
+        this.getThing().setPropertyWriteHandler("isOn", async (newValue) => {
+            if (typeof newValue === "boolean") {
+                this.isOn = newValue;
+                console.log(`Change state in: ${this.isOn}`);
+            } else {
+                throw new Error("Invalid state value. Must be a boolean");
+            }
+        });
+
+        this.thing.setActionHandler("toggle", async () => {
+            if (this.isOn) {
+                this.isOn = false;
+            } else {
+                this.isOn = true;
+            } 
+            return undefined;
+        });
     }
 
     public tickEvent(): void {
-        this.intensity += 1;
-        if (this.intensity > 100) this.intensity = 100; 
-        console.log(`Tick action for ${this.getThing().title}, intensity increased to: ${this.intensity}`);
+        if (this.isOn) {
+            this.intensity += 1;
+            if (this.intensity > 100) this.intensity = 100; 
+            console.log(`Tick action for ${this.getThing().title}, intensity increased to: ${this.intensity}`);
+        }
     }
 }
