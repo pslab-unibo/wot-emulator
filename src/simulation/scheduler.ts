7f8264715e713a1b1a54383a1095a2209160306d
matidan01 <matildedantino@gmail.com>
2024-11-14 17:57:28 +0100

    Add environment

--- src/simulation/scheduler.ts
+++ src/simulation/scheduler.ts
@@ -1,21 +1,25 @@
-import { DefaultContent, Thing } from "@node-wot/core";
-import { Readable } from "stream";
-import { ThingInterface } from "../thing-model/ThingInterface";
+import { Thing } from "../thing-model/Thing";
 import { eventQueue } from '../simulation/eventQueue';
 
 // Scheduler class to manage periodic actions on Things and process event commands
 export class Scheduler {
 
     private period: number;
-    private things: ThingInterface[] = [];
+    private environment? : Thing;
+    private things: Thing[] = [];
 
     constructor(period: number) {
         this.period = period;
     }
 
-    public addThing(thing: ThingInterface): void {
+    public addThing(thing: Thing): void {
         this.things.push(thing);
         console.log(`Thing added: ${thing.getThing().title}`);
+    }
+
+    public setEnvironment(env : Thing) {
+        console.log("Set environment");
+        this.environment = env;
     }
 
     public async start(): Promise<void> {
@@ -24,6 +28,10 @@ export class Scheduler {
         while (true) {
             // Processes queued events asynchronously
             await eventQueue.processQueue();
+
+            if(this.environment) {
+                this.environment.tick();
+            }
 
             // Iterates through each Thing to invoke the 'update' if it exists
             for (const thing of this.things) {
