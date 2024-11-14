04e91ab6f41f4d77fe2ff75ea251b36b39dd2023
matidan01 <matildedantino@gmail.com>
2024-11-14 17:57:09 +0100

    Add environment and radiator

--- src/simulation/init.ts
+++ src/simulation/init.ts
@@ -3,13 +3,21 @@ import { Scheduler } from "./scheduler";
 import { LampThing } from "../thing-model/things/LampThing";
 import * as fs from 'fs';
 import { ServientManager } from "./ServientManager";
-import { ThingInterface } from "../thing-model/ThingInterface";
+import { Thing } from "../thing-model/Thing";
+import { HeatingEnv } from "../thing-model/environments/HeatingEnv";
+import { Radiator } from "../thing-model/things/Radiator";
 
 // Factory function to create a Thing instance based on its type
-function createThingByType(type: string, period: number, servient: Servient, init: WoT.ExposedThingInit) : ThingInterface {
+function createThingByType(type: string, servient: Servient, init: WoT.ExposedThingInit, environment : Thing, power? : number, period?: number,) : Thing {
     switch (type) {
         case "LampThing":
-            return new LampThing(servient, init, period);
+            if(period) {
+                return new LampThing(servient, init, environment, period);
+            }
+        case "Radiator":
+            if(power) {
+                return new Radiator(servient, init, environment, power);
+            }
         default:
             throw new Error(`Unsupported Thing type: ${type}`);
     }
@@ -18,10 +26,20 @@ function createThingByType(type: string, period: number, servient: Servient, ini
 export async function initializeThings(scheduler: Scheduler) {
     // Reads configuration data for Things from a JSON file
     const thingsData = JSON.parse(fs.readFileSync('./src/td/things.json', 'utf8'));
+    const envData = JSON.parse(fs.readFileSync('./src/td/environment.json', 'utf8'));
 
     // Creates an instance of ServientManager to manage multiple servients
     // SINGLETON ??
     const servients : ServientManager = new ServientManager(thingsData);
+
+    const serv = servients.getServient(0)
+    var env = undefined;
+    if(serv) {
+        env = new HeatingEnv(serv, envData, envData.volume, envData.ambientTemperature, envData.temperature, envData.coolingConstant);
+        scheduler.setEnvironment(env);
+        console.log(env.getThing().getThingDescription());
+        env.getThing().expose();
+    }
 
     for (const thingConfig of thingsData) {
         try {
@@ -32,10 +50,14 @@ export async function initializeThings(scheduler: Scheduler) {
             const servient = servients.getServient(thingConfig.servient) || servients.getServient(0);
             
             if (servient) {
-
+                var thing = undefined;
                 // Creates the Thing based on its type and attaches it to the servient
-                const thing = createThingByType(thingType, period, servient, thingConfig);
-
+                if (thingConfig.power) {
+                    thing = createThingByType(thingType, servient, thingConfig, env as Thing, thingConfig.power);
+                } else {
+                    thing = createThingByType(thingType, servient, thingConfig, env as Thing, undefined, period);
+                }
+                
                 try {
                     // Exposes the Thing to make it available for interaction
                     await thing.getThing().expose();
