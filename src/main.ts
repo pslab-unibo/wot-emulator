200b3bd16d91e94a777a9bd57227f4a1b1d2ff9e
matidan01 <matildedantino@gmail.com>
2024-11-15 17:04:52 +0100

    Delete commandListener

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { Scheduler } from "./simulation/scheduler";
-import { startCommandListener } from "./command/commandListener";
 import { initializeThings } from "./simulation/init";
 
 // Instantiate the Scheduler
 const scheduler = new Scheduler(100);
-
-// Start listening for external commands through the command listener
-startCommandListener(scheduler);
 
 // Initialize Things by reading configuration and exposing them via the specified Servient
 initializeThings(scheduler);
