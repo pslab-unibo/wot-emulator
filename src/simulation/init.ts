447b1c9fac9eb940f66b61b2aeb699033bb0037d
matidan01 <matildedantino@gmail.com>
2024-11-16 20:52:09 +0100

    Refactored initialization functions

--- src/simulation/init.ts
+++ src/simulation/init.ts
@@ -3,37 +3,55 @@ import * as fs from 'fs';
 import { ServientManager } from "./ServientManager";
 import { Thing } from "../thing-model/Thing";
 
-export async function initializeThings(scheduler: Scheduler) {
+export async function initialize(scheduler: Scheduler) {
+    const servientConfig = JSON.parse(fs.readFileSync('./src/td/config.json', 'utf8')).servients;
 
-    // Reads configuration data for Things from a JSON file
+    // Creates an instance of ServientManager to manage multiple servients
+    const servients : ServientManager = new ServientManager(servientConfig);
+
+    initializeEnvironment(scheduler, servients)
+            .then((environment) => initializeThings(scheduler, servients, environment));
+    
+}
+
+async function initializeEnvironment(scheduler: Scheduler, servients: ServientManager): Promise<Thing> {
     const configData = JSON.parse(fs.readFileSync('./src/td/config.json', 'utf8'));
     const envConfig = configData.environment;
 
-    // Creates an instance of ServientManager to manage multiple servients
-    const servients : ServientManager = new ServientManager(configData.servients);
+    const servient = servients.getServient(envConfig.servient);
 
-    const serv = servients.getServient(0);
-    var env = undefined;
-    
-    if(serv) {
-        const envModule = await import(`../thing-model/environments/${envConfig[0].type}`);
-        env = envModule.create(serv, envConfig, new Map(Object.entries(envConfig[0])));
-        scheduler.setEnvironment(env);
-        env.getThing().expose();
+    if (!servient) {
+        throw new Error("No servient found for the environment configuration.");
     }
 
-    for (const thingConfig of configData.things) {
+    try {
+        const envModule = await import(`../thing-model/environments/${envConfig[0].type}`);
+        const environment = envModule.create(servient, envConfig, new Map(Object.entries(envConfig[0])));
+        scheduler.setEnvironment(environment);
+        await environment.getThing().expose();
+        return environment;
+    } catch (error) {
+        console.error("Failed to initialize environment:", error);
+        throw error; 
+    }
+}
+
+
+async function initializeThings(scheduler: Scheduler, servients : ServientManager, environment : Thing) {
+    const configData = JSON.parse(fs.readFileSync('./src/td/config.json', 'utf8')).things;
+    
+    for (const thingConfig of configData) {
         try {
             const thingType = thingConfig.type;
             const period = thingConfig.period;
             
             // Retrieves the servient based on the Thing configuration; defaults to servient with ID 0 if undefined
-            const servient = servients.getServient(thingConfig.servient) || servients.getServient(0);
+            const servient = servients.getServient(thingConfig.servient);
             
             if (servient) {
                 
                 const thingModule = await import(`../thing-model/things/${thingType}`);
-                const thing = thingModule.create(servient, thingConfig, env as Thing, period, new Map(Object.entries(thingConfig)))
+                const thing = thingModule.create(servient, thingConfig, environment, period, new Map(Object.entries(thingConfig)))
 
                 // Exposes the Thing to make it available for interaction
                 await thing.getThing().expose();
