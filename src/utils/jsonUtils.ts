e4c5ab0f69800c84a9348835f2ac8620c720711b
matidan01 <matildedantino@gmail.com>
2024-12-18 22:43:43 +0100

    Make utils

---
+++ src/utils/jsonUtils.ts
@@ -0,0 +1,36 @@
+import { Thing } from "../thing-model/Thing";
+
+export function generateJson(things: Thing[], environment?: Thing) {
+    if (environment) {
+        things = [environment, ...things];
+    }
+    const thingsJson = things.map(thing => JSON.parse(thing.toString()));
+    return thingsJson;
+}
+
+export function generatePatch(json1: any[], json2: any[]) {
+    const patch: any[] = [];
+
+    for (let i = 0; i < json1.length; i++) {
+        const thing1 = json1[i];
+        const thing2 = json2[i];
+
+        if (thing1.title !== thing2.title) {
+            throw new Error(`Titles do not match for index ${i}: "${thing1.title}" vs "${thing2.title}"`);
+        }
+
+        const changes: any = { "title": thing1.title };
+
+        Object.keys(thing1).forEach(key => {
+            if (key !== "title" && thing1[key] !== thing2[key]) {
+                changes[key] = thing2[key]; 
+            }
+        });
+
+        if (Object.keys(changes).length > 1) {
+            patch.push(changes);
+        }
+    }
+
+    return patch;
+}
