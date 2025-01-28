84fd3b64d9b05126b27a05cd657154af3cf9d038
matidan01 <matildedantino@gmail.com>
2025-01-28 15:58:04 +0100

    Change td and thing-model name with 'configuratione' e 'thing-implementation'

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { inizializeServer } from "./simulation/server";
 
 // Path to the main configuration file for the simulator, defining the setup for servients, environments, and things.
-export const CONFIG = "./src/td/configMuseum.json";
+export const CONFIG = "./src/configuration/configMuseum.json";
 
 // Path to the directory containing models for the various SituatedThings available in the simulation.
-export const THING_MODEL = "../thing-model/things/museum/";
+export const THING_MODEL = "../thing-implementation/things/museum/";
 
 // Path to the directory containing models for the different environments used in the simulation.
-export const ENV_MODEL = "../thing-model/environments/museum/";
+export const ENV_MODEL = "../thing-implementation/environments/museum/";
 
 inizializeServer();
 
